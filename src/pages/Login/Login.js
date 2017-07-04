d983f11ef4915979ff2c636bbf7a69bc265cb7c3
kobayang <naokiod@gmail.com>
2017-07-04 18:45:59 +0900

    Create signup and login tmp pages

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,13 +1,29 @@
 import React, {Component} from 'react';
+import {Container, Form, Button} from 'semantic-ui-react';
 import {Header} from 'components';
+import styles from './Login.css';
 
 export default class Login extends Component {
   render() {
     return (
       <div className="page">
         <Header />
-        <h1>This is login page</h1>
-        <h2>TODO</h2>
+        <Container>
+          <div className="login" style={styles}>
+            <h1>ログイン</h1>
+            <Form>
+              <Form.Field>
+                <label>ユーザー名</label>
+                <input placeholder='User Name' />
+              </Form.Field>
+              <Form.Field>
+                <label>パスワード</label>
+                <input placeholder='Password' />
+              </Form.Field>
+              <Button type='submit'>ログイン</Button>
+            </Form>
+          </div>
+        </Container>
       </div>
     )
   }
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import {Container, Form, Button} from 'semantic-ui-react';
 import {Header} from 'components';
+import styles from './Signup.css';
 
 export default class Signup extends Component {
   render() {
     return (
       <div className="page">
         <Header />
-        <h1>This is signup page</h1>
-        <h2>TODO</h2>
+        <Container>
+          <div className="signup" style={styles}>
+            <h1>サインアップ</h1>
+            <Form>
+              <Form.Field>
+                <label>ユーザー名</label>
+                <input placeholder='User Name' />
+              </Form.Field>
+              <Form.Field>
+                <label>パスワード</label>
+                <input placeholder='Password' />
+              </Form.Field>
+              <Form.Field>
+                <label>確認</label>
+                <input placeholder='Confirmation' />
+              </Form.Field>
+              <Button type='submit'>登録</Button>
+            </Form>
+          </div>
+        </Container>
       </div>
     )
   }
