16f4573e747afe1b47b22525437e04c6f9a0c649
kobayang <naokiod@gmail.com>
2017-07-05 20:21:31 +0900

    update header layout to add container in menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,23 @@
 import React, {Component} from 'react';
-import { Button, Menu } from 'semantic-ui-react';
+import {Button, Menu, Container} from 'semantic-ui-react';
 import {Link} from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
     return (
       <Menu size="large">
-        <Menu.Item as={Link} to="/" name='Barry' />
-        
-        <Menu.Menu position="right">
-          <Menu.Item>
-            <Button as={Link} to="/signup" color="orange">Sign Up</Button>
-          </Menu.Item>
-          <Menu.Item>
-            <Button as={Link} to="/login">Log in</Button>
-          </Menu.Item>
-        </Menu.Menu>
+        <Container>
+          <Menu.Item as={Link} to="/" name='Barry' />
+          
+          <Menu.Menu position="right">
+            <Menu.Item>
+              <Button as={Link} to="/signup" color="orange">Sign Up</Button>
+            </Menu.Item>
+            <Menu.Item>
+              <Button as={Link} to="/login">Log in</Button>
+            </Menu.Item>
+          </Menu.Menu>
+        </Container>
       </Menu>
     );
   }
