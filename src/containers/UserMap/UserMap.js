42780c7867d5778af07aa792dedb07bc5e5b0828
kobayang <naokiod@gmail.com>
2017-07-13 10:14:50 +0900

    Add empty UserMap container

---
+++ src/containers/UserMap/UserMap.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class UserMap extends Component {
+  
+  render() {
+    return (
+      <div>Implementing...</div>
+    )
+  }
+}
+
+export default UserMap;
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,4 +1,6 @@
 import PrivateRoute from './Routes/Private';
 import GuestRoute from './Routes/Guest';
+import UserMap from './UserMap/UserMap';
+import UserHeader from './UserHeader/UserHeader';
 
-export {PrivateRoute, GuestRoute};
+export {PrivateRoute, GuestRoute, UserMap, UserHeader};
