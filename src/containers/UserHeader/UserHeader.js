ccc3ee96222f0c3c34134b67aff180f6ab19854c
kobayang <naokiod@gmail.com>
2017-07-21 11:13:21 +0900

    Create moveable sidebar

--- src/containers/UserHeader/UserHeader.js
+++ src/containers/UserHeader/UserHeader.js
@@ -3,8 +3,7 @@ import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import {withRouter} from 'react-router';
-import {Dropdown, Menu, Container, Button, Icon} from 'semantic-ui-react';
-import {Link} from 'react-router-dom';
+import {Dropdown, Menu, Button, Icon} from 'semantic-ui-react';
 import * as authActions from 'redux/modules/auth';
 
 class UserHeader extends Component {
@@ -21,23 +20,22 @@ class UserHeader extends Component {
   render() {
     return (
       <Menu size="large" compact={true}>
-        <Container>
-          <Menu.Item as={Link} to="/user" name="Barry" />
-          
-          <Menu.Menu position="right">
-            <Dropdown text={this.props.user.email} pointing className='link item'>
-              <Dropdown.Menu>
-                <Dropdown.Item>
-                  <Icon name="setting" />
-                  User Edit
-                </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-            <Menu.Item>
-              <Button onClick={this.hundleSignout}>Sign out</Button>
-            </Menu.Item>
-          </Menu.Menu>
-        </Container>
+        <Menu.Item onClick={this.props.parentFunc}>
+          <Icon name="content" size="large" />
+        </Menu.Item>
+        <Menu.Menu position="right">
+          <Dropdown text={this.props.user.email} pointing className='link item'>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <Icon name="setting" />
+                User Edit
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+          <Menu.Item>
+            <Button onClick={this.hundleSignout}>Sign out</Button>
+          </Menu.Item>
+        </Menu.Menu>
       </Menu>
     )
   }
--- src/pages/User/User.js
+++ src/pages/User/User.js
@@ -1,16 +1,46 @@
 import React, {Component} from 'react';
 import style from './User.css';
 import {UserHeader, UserMap} from 'containers';
+import {Sidebar, Menu, Icon} from 'semantic-ui-react'
+
 
 export default class User extends Component {
+  constructor() {
+    super();
+    this.state = {
+      visible: false
+    };
+  };
+  
+  toggleHeaderVisible = (event) => {
+    event.preventDefault();
+    this.setState({visible: !this.state.visible});
+  };
+  
   render() {
     return (
       <div className="page" style={style}>
-        <UserHeader />
-        <section className="main">
-          <UserMap />
-        </section>
+        <UserHeader parentFunc={this.toggleHeaderVisible}/>
+        <Sidebar.Pushable className="main">
+          <Sidebar as={Menu} animation='overlay' direction='left' visible={this.state.visible} icon='labeled' width="wide" vertical>
+            <Menu.Item name='home'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item name='gamepad'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item name='camera'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+          <Sidebar.Pusher>
+            <UserMap />
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
       </div>
     )
-  }
+  };
 }
