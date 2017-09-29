b7499c7c604d02b440119ff5c4356b4eb29913f5
rerost <gmailii@yahoo.co.jp>
2017-09-29 11:18:18 +0900

    add coordinates

--- src/containers/CommentPostView/CommentPostView.js
+++ src/containers/CommentPostView/CommentPostView.js
@@ -8,10 +8,10 @@ import * as mapActions from 'redux/modules/map';
 import {Button, Form} from 'semantic-ui-react'
 
 import Comment from 'model/comment';
+import {DateFactory} from 'helpers/date';
 
 class CommentPostView extends Component {
   constructor() {
-    console.log("called")
     super()
     this.state = {
       comment: ""
@@ -27,15 +27,20 @@ class CommentPostView extends Component {
     return (
       <Form reply>
         <Form.TextArea onChange={(e, hash) => this.handleChange(e, hash)}/>
-        <Button content='Add Comment' labelPosition='left' icon='edit' onClick={ () => {console.log(this.state); Comment.push(this.props.user, this.state.comment)} } primary />
+        <Button content='Add Comment' labelPosition='left' icon='edit' onClick={ () => {Comment.push(this.props.user, this.state.comment, this.props.coordinates)} } primary />
       </Form>
     )
   }
 };
 
 const mapStateToProps = (state) => {
+  const {coordinates: currentCoordinates, selectedCoordinates, selectedDay} = state.map
+  const isToday = DateFactory.today() === selectedDay;
+  // 過去に行った場所にコメントできる必要はあるか微妙。UIに合わせた
+  const coordinates = isToday ? currentCoordinates : selectedCoordinates
   return {
     user: state.auth.user,
+    coordinates,
   };
 };
 
