f1fce43361c02cbe3d068ca870f6686f8f5c8ab4
kobayang <naokiod@gmail.com>
2017-11-07 18:38:59 +0900

    Add formated date to bow-waws

--- src/components/PopupComment/PopupComment.js
+++ src/components/PopupComment/PopupComment.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import { Card } from "semantic-ui-react";
+import { formatDate } from "helpers/format";
 import "./PopupComment.css";
 
 export default class PopupComment extends Component {
   render() {
+    const { bow } = this.props;
+    const created_at = formatDate(bow.created_at);
     return (
       <Card.Group className="comment-popup">
         <Card>
           <Card.Content>
-            <Card.Header>{this.props.name}</Card.Header>
-            <Card.Description>{this.props.value}</Card.Description>
+            <Card.Header>{bow.user.screenName}</Card.Header>
+            <Card.Meta>{created_at}</Card.Meta>
+            <Card.Description>{bow.comment}</Card.Description>
           </Card.Content>
         </Card>
       </Card.Group>
